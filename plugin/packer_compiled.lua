27cc80fb27e0d7daaccc9264ee6ed11c692d17a0
Gabeh98 <bielhenry09@gmail.com>
2023-11-22 21:01:50 -0300

    add new set mod

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -144,6 +144,11 @@ _G.packer_plugins = {
     path = "/home/gabe/.local/share/nvim/site/pack/packer/start/tokyonight.nvim",
     url = "https://github.com/folke/tokyonight.nvim"
   },
+  ["vim-airline"] = {
+    loaded = true,
+    path = "/home/gabe/.local/share/nvim/site/pack/packer/start/vim-airline",
+    url = "https://github.com/vim-airline/vim-airline"
+  },
   ["vim-fugitive"] = {
     loaded = true,
     path = "/home/gabe/.local/share/nvim/site/pack/packer/start/vim-fugitive",
